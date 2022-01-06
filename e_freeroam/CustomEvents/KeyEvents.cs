02d544952690e655c3f680e8849f5218dff019e3
Jacob Tracey <42984369+Jacob-Tr@users.noreply.github.com>
2022-01-06 17:09:44 -0400

    Add files via upload

---
+++ e_freeroam/CustomEvents/KeyEvents.cs
@@ -0,0 +1,20 @@
+﻿using GTANetworkAPI;
+using System.Reflection;
+
+namespace e_freeroam.CustomEvents
+{
+    class KeyEvents : Script
+    {
+        [RemoteEvent("OnKeyPress")]
+        public void KeyPressEvent(Player player, int key)
+        {
+            Utilities.ChatUtils.sendClientMessage(player, Utilities.ChatUtils.getColorAsHex(Utilities.ServerUtils.ServerData.COLOR_WANTED_BLUE), "" + key);
+        }
+
+        [RemoteEvent("OnKeyRelease")]
+        public void KeyReleaseEvent(Player player, int key)
+        {
+            Utilities.ChatUtils.sendClientMessage(player, Utilities.ChatUtils.getColorAsHex(Utilities.ServerUtils.ServerData.COLOR_WANTED_BLUE), "" + key);
+        }
+    }
+}
